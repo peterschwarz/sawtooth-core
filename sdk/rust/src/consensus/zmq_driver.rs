76e31194bc7e43c2bdaa7ad42933211de7eeecd3
Peter Schwarz <pschwarz@bitwise.io>
2018-06-15 15:04:48 -0500

    Add sleep after not ready registration
    Add a sleep after receiving a not ready response from the validator.
    This will prevent the consensus engine from overwhelming the
    validator while trying to register before the validator has a received a
    genesis block.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- sdk/rust/src/consensus/zmq_driver.rs
+++ sdk/rust/src/consensus/zmq_driver.rs
@@ -170,6 +170,7 @@ pub fn register(
                         break;
                     }
                     ConsensusRegisterResponse_Status::NOT_READY => {
+                        ::std::thread::sleep(::std::time::Duration::from_millis(1500));
                         msg = sender
                             .send(
                                 Message_MessageType::CONSENSUS_REGISTER_REQUEST,
