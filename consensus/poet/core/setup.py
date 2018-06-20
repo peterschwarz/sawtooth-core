f84476f52978d187ecde22ee8a016de95cb45451
Boyd Johnson <bjohnson@bitwise.io>
2018-06-20 09:39:22 -0500

    Modify packaging for poet-core to not depend on validator
    Poet-core no longer imports from validator.
    
    Signed-off-by: Boyd Johnson <bjohnson@bitwise.io>

--- consensus/poet/core/setup.py
+++ consensus/poet/core/setup.py
@@ -32,6 +32,7 @@ setup(name='sawtooth-poet-core',
           'sawtooth-poet-common',
           'sawtooth-poet-simulator',
           'sawtooth-signing',
-          'sawtooth-validator',
+          'cbor',
+          'lmdb',
       ],
       entry_points={})
